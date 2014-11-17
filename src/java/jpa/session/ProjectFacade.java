c9ec311a4528123a3ec39744c93b60ae4dd873b6
wilso_000 <wilson_novido@yahoo.com>
2014-11-17 09:59:55 -0500

    Initial commit.

---
+++ src/java/jpa/session/ProjectFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.session;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import jpa.entities.Project;
+
+/**
+ *
+ * @author wilso_000
+ */
+@Stateless
+public class ProjectFacade extends AbstractFacade<Project> {
+    @PersistenceContext(unitName = "ConsultingAgencyPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProjectFacade() {
+        super(Project.class);
+    }
+    
+}
